24957ed63cd67266fb350b808fe82284cd312432
newbraham <du.okagabriel@gmail.com>
2026-05-11 21:38:57 -0300

    feat(crypto): add ECDH key pair generation with secp256r1 curve

---
+++ src/main/java/com/nodeconnect/crypto/KeyManager.java
@@ -0,0 +1,56 @@
+package com.nodeconnect.crypto;
+
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+
+
+public class KeyManager {
+    private static final String CURVE_NAME = "secp256k1";
+
+    private final KeyPair localKeyPair;
+    private PublicKey peerPublicKey;
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    private KeyPair generateKeyPair() throws GeneralSecurityException {
+        ECNamedCurveParameterSpec spec = ECNamedCurveTable.getParameterSpec(CURVE_NAME);
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("EC", "BC");
+        gen.initialize(spec);
+        return gen.generateKeyPair();
+    }
+
+    public KeyManager() throws GeneralSecurityException {
+        this.localKeyPair = generateKeyPair();
+    }
+
+    public  PublicKey getLocalPublicKey() {
+        return localKeyPair.getPublic();
+    }
+
+    public PrivateKey getLocalPrivateKey() {
+        return localKeyPair.getPrivate();
+    }
+
+    public void setPeerPublicKey(PublicKey peerPublicKey) {
+        this.peerPublicKey = peerPublicKey;
+    }
+
+    public PublicKey getPeerPublicKey() {
+        return peerPublicKey;
+    }
+
+    public boolean isHandshakeComplete() {
+        return peerPublicKey != null;
+    }
+
+}
